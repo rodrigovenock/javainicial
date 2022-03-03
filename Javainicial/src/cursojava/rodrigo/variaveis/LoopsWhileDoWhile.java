46adf5f7cae93ed2771e0b88bdaba59bfef7028d
U_U <U_U@192.168.0.13>
2022-03-02 21:27:38 -0300

    Loop com While

---
+++ Javainicial/src/cursojava/rodrigo/variaveis/LoopsWhileDoWhile.java
@@ -0,0 +1,19 @@
+package cursojava.rodrigo.variaveis;
+
+public class LoopsWhileDoWhile {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		int max = 10;
+		
+		System.out.println("Contando até " + max);
+		
+		while (i <= max) {
+			System.out.println("Valor de i: " + i);
+			i++;
+		}
+
+	}
+
+}
