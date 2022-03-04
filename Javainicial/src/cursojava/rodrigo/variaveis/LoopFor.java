f99bee3852fe4d6073473d6f26821ea45b4e8e81
U_U <U_U@192.168.0.13>
2022-03-04 17:38:30 -0300

    Loop For

---
+++ Javainicial/src/cursojava/rodrigo/variaveis/LoopFor.java
@@ -0,0 +1,11 @@
+package cursojava.rodrigo.variaveis;
+
+public class LoopFor {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i <= 5; i++) {
+			System.out.println("i tem valor: " + i);
+		}
+	}
+}
